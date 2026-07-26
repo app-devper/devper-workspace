396526c458ae7e15c7a73b0fe1f4504cf71b2bcc
worawit <worawit.bs@gmail.com>
2026-07-26 22:55:04 +0700

    chore(pos): remove unused open file plugin

--- packages/applications/pos/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ packages/applications/pos/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -36,11 +36,6 @@ public final class GeneratedPluginRegistrant {
       Log.e(TAG, "Error registering plugin jni_flutter, com.github.dart_lang.jni_flutter.JniFlutterPlugin", e);
     }
     try {
-      flutterEngine.getPlugins().add(new com.crazecoder.openfile.OpenFilePlugin());
-    } catch (Exception e) {
-      Log.e(TAG, "Error registering plugin open_file_android, com.crazecoder.openfile.OpenFilePlugin", e);
-    }
-    try {
       flutterEngine.getPlugins().add(new net.touchcapture.qr.flutterqr.FlutterQrPlugin());
     } catch (Exception e) {
       Log.e(TAG, "Error registering plugin qr_code_scanner, net.touchcapture.qr.flutterqr.FlutterQrPlugin", e);
